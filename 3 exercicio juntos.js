809c2da9ed8ad4446cdc2a9375af0cbc90af63a0
castrosd842-spec <castrosd842@gmail.com>
2026-03-23 20:24:17 -0300

    m

---
+++ 3 exercicio juntos.js
@@ -0,0 +1,31 @@
+// =============================
+// 1. Área de um retângulo
+// =============================
+let largura = 10;
+let altura = 5;
+
+let area = largura * altura;
+
+console.log("Área do retângulo:", area);
+
+
+// =============================
+// 2. Comparar duas variáveis
+// =============================
+let a = 10;
+let b = 20;
+
+console.log("a é igual a b?", a == b);
+console.log("a é diferente de b?", a != b);
+console.log("a é maior que b?", a > b);
+console.log("a é menor que b?", a < b);
+
+
+// =============================
+// 3. Celsius para Fahrenheit
+// =============================
+let celsius = 25;
+
+let fahrenheit = (celsius * 9/5) + 32;
+
+console.log("Temperatura em Fahrenheit:", fahrenheit);
